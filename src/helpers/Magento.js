fd5b0e5a1f04716058bb6d3c4ca6e431f730fd7f
ryntab <57367794+ryntab@users.noreply.github.com>
2024-07-30 00:19:38 -0400

    feat: :mag: make magento theme finder more robust

--- src/helpers/Magento.js
+++ src/helpers/Magento.js
@@ -17,6 +17,37 @@ const Magento_Helpers = {
                 }
             });
         }
+
+        if (themes.length === 0) {
+            regex = /\/static\/version([\w-]+)\/frontend\/([\w-]+)\/([\w-]+)/ig;
+            matches = html.match(regex);
+            if (matches) {
+                matches.forEach(match => {
+                    let theme = match.split('/')[4];
+                    if (!themes.includes(theme)) {
+                        themes.push(theme);
+                    }
+                });
+            }
+        }
+
+        if (themes.length === 0) {
+            regex = /\/static\/frontend\/([\w-]+)\/([\w-]+)/ig;
+            matches = html.match(regex);
+            if (matches) {
+                matches.forEach(match => {
+                    let theme = match.split('/')[3];
+                    if (!themes.includes(theme)) {
+                        themes.push(theme);
+                    }
+                });
+            }
+        }
+
+        if (themes.length === 0) {
+           return false;
+        }
+
         const end = performance.now();
         return {
             url: url,
